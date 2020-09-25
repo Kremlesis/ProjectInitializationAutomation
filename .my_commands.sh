6b40b67d34b84860074ed77c07b9954575a630a7
Simarpreet Singh Saluja <s1simarpreet@gmail.com>
2020-09-25 06:03:40 +0530

    Private/Public Added

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 function create() {
-    cd
     source .env
-    python create.py $1
+    python create.py $1 $2
     cd $FILEPATH$1
     git init
     git remote add origin git@github.com:$USERNAME/$1.git
     touch README.md
     git add .
-    git commit -m "Initial commit"
+    git commit -m "README Created"
     git push -u origin master
-    code .
 }
--- create.py
+++ create.py
@@ -11,9 +11,13 @@ password = os.getenv("PASSWORD")
 
 def create():
     folderName = str(sys.argv[1])
+    if sys.argv[2] == "private":
+        private = True
+    else:
+        private = False
     os.makedirs(path + str(folderName))
     user = Github(username, password).get_user()
-    repo = user.create_repo(folderName)
+    repo = user.create_repo(folderName,private=private)
     print("Succesfully created repository {}".format(folderName))
 
 if __name__ == "__main__":
